077380e5eabbdb2dc2b83ebf1cf4f0ccd198753f
Максим Денисов <max.denis0v@protonmail.com>
2022-08-24 00:23:23 +0300

    UniqueTogetherValidator

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from reviews.models import Categories, Genres, Title
 from rest_framework.validators import UniqueTogetherValidator
+from reviews.models import Categories, Genres, Title
 
 
 class UserSerializer(serializers.ModelSerializer):
