9b9e4c0a14a58daf1a3184e26ed9eac8cd9d97b0
Максим Денисов <max.denis0v@protonmail.com>
2022-08-24 20:20:14 +0300

    Черновые сериализаторы и вьюхи

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,4 +1,23 @@
 from rest_framework import serializers
+from reviews.models import Categories, Genre, Title
+
+
+class CategoriesSerializers(serializers.ModelSerializer):
+    class Meta:
+        fields = ('name',)
+        model = Categories
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('name',)
+        model = Genre
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = '__all__'
+        model = Title
 
 
 from reviews.models import Categories, Genres, Title
