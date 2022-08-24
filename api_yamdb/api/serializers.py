ae8ab9a9facc97079b29a72fc5ea8d91f9cf0886
Александр Дементьев <dementev.aleksander@gmail.com>
2022-08-24 15:18:59 +0300

    Создана модель Reviews и Comments, добавлен вывод в админ.панель, созданы views, serializers

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
+
+
 from reviews.models import Categories, Genres, Title
+from rest_framework.validators import UniqueTogetherValidator
 from rest_framework.validators import UniqueTogetherValidator
 from reviews.models import Categories, Genres, Title
 
