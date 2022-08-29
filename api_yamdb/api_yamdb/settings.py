20fa673114102e1f5a8370f4d34d7d7848d34b5c
EvgeniyaZay <zhenyayaya121996@gmail.com>
2022-08-29 22:56:04 +0300

    внесение правок для отправи на ревью

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -23,7 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_simplejwt',
+    'djoser',
     'django_filters',
     'reviews',
     'api',
